1a17760c303793b4dda27fa51b05172505cbb903
yasirabd <sir.yasirabd@gmail.com>
2024-01-29 08:23:47 +0700

    belajar optional parameter

--- 01 - Dart/belajar_dart/bin/function.dart
+++ 01 - Dart/belajar_dart/bin/function.dart
@@ -1,8 +1,9 @@
-void sayHello(String firstName, String lastName) {
+void sayHello(String firstName, [String? lastName]) {
   print('Hello World!');
   print('Hello $firstName $lastName');
 }
 
 void main() {
   sayHello('Yasir', 'Abdur Rohman');
+  sayHello('Yasir');
 }
