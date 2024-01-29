68b30af145be6da7e80fcb5fe24a47c78b944221
yasirabd <sir.yasirabd@gmail.com>
2024-01-29 08:12:37 +0700

    belajar loop while

---
+++ 01 - Dart/belajar_dart/bin/loop_while.dart
@@ -0,0 +1,13 @@
+void main() {
+  var counter = 1;
+  while (counter <= 10) {
+    print('Perulangan ke-$counter');
+    counter++;
+  }
+
+  var c = 1;
+  do {
+    print('Perulangan ke-$c');
+    c++;
+  } while (c <= 10);
+}
