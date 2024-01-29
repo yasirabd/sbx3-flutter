9c27ed3d22e818729ef7e8c4565fdc04aa9b0c34
yasirabd <sir.yasirabd@gmail.com>
2024-01-29 07:43:56 +0700

    belajar switch_case.dart

---
+++ 01 - Dart/belajar_dart/bin/switch_case.dart
@@ -0,0 +1,16 @@
+void main() {
+  var nilai = 'F';
+  switch (nilai) {
+    case 'A':
+      print('Mumtaz');
+      break;
+    case 'B':
+      print('Jayyid');
+      break;
+    case 'C':
+      print('Rasib');
+      break;
+    default:
+      print('Naqish');
+  }
+}
