670c4dbc9c2693662c778306d5141fc99df59f21
yasirabd <sir.yasirabd@gmail.com>
2024-02-27 19:18:21 +0700

    rename folder & add project flutter UI welcome view

---
+++ 02-Flutter/ajheryuk/android/app/src/main/kotlin/com/yasirabd/ajheryuk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yasirabd.ajheryuk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 02-Flutter/ajheryuk/lib/app/modules/welcome/views/welcome_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/welcome_controller.dart';
+
+class WelcomeView extends GetView<WelcomeController> {
+  const WelcomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(14),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: Get.width * 0.5,
+              height: Get.height * 0.2,
+              child: Image.asset(
+                "assets/logo/logo.png",
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            const Text(
+              "Welcome to Ajheryuk",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Text(
+              "Best and popular apps for live education course from home",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18, color: Color(0xFF8C8C8C)),
+            ),
+            const SizedBox(
+              height: 75,
+            ),
+            Container(
+              width: Get.width,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color(0xFFEC5F5F),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                  ),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                ),
+                child: const Text("Get started"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
