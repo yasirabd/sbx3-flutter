bd608659dcdcb6798ad5af432e070400d8ad1c8f
yasirabd <sir.yasirabd@gmail.com>
2024-02-28 12:03:08 +0700

    add carousel slider

--- 02-Flutter/ajheryuk/lib/app/modules/home/controllers/home_controller.dart
+++ 02-Flutter/ajheryuk/lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,7 @@
+import 'package:carousel_slider/carousel_controller.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
+  var current = 0.obs;
+  CarouselController carouselController = CarouselController();
 }
